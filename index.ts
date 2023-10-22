05a1e328e1ac53a1121c3d1283964d57df79df38
Sam Roberts <sam.roberts.1983@gmail.com>
2023-10-22 22:04:53 +1000

    Define an initial lambda

--- index.ts
+++ index.ts
@@ -40,3 +40,53 @@ const budget = new aws.budgets.Budget("MonthlyCostBudget", {
     timePeriodStart: "2023-10-01_00:00",
     timeUnit: "MONTHLY",
 });
+
+const lambdaExecutionAssumeRolePolicy = aws.iam.getPolicyDocument({
+    statements: [{
+        effect: "Allow",
+        principals: [{
+            type: "Service",
+            identifiers: ["lambda.amazonaws.com"],
+        }],
+        actions: ["sts:AssumeRole"],
+    }],
+});
+
+const lambdaExecutionPolicy = aws.iam.getPolicyDocument({
+    statements: [{
+        effect: "Allow",
+        actions: [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+        ],
+        resources: ["arn:aws:logs:*:*:*"], // TODO can I lock this down a bit more?
+    }],
+});
+
+const lambdaExecutionRole = new aws.iam.Role("FuzzyLamdaExecutionRole", {
+    name: "FuzzyLambdaExecutionRole",
+    assumeRolePolicy: lambdaExecutionAssumeRolePolicy.then(policy => policy.json),
+    inlinePolicies: [{
+        name: "FuzzyLambdaExecutionPolicy",
+        policy: lambdaExecutionPolicy.then(policy => policy.json),
+    }],
+});
+
+const lambda = new aws.lambda.Function("FuzzyLambda", {
+    name: "FuzzyLambda",
+    packageType: "Zip",
+    // TODO how to make this code reference more robust?
+    code: new pulumi.asset.FileArchive("../fuzzy_rust/target/lambda/fuzzy_lambda/bootstrap.zip"),
+    handler: "bootstrap",
+    role: lambdaExecutionRole.arn,
+    runtime: "provided.al2",
+    architectures: ["arm64"],
+});
+
+const lambdaUrl = new aws.lambda.FunctionUrl("FuzzyLambdaUrl", {
+    functionName: lambda.arn,
+    authorizationType: "AWS_IAM",
+});
+
+export const url = lambdaUrl.functionUrl;
