0767dd1df5468538be0c9ed1a8d6426066242c9e
Sam Roberts <sam.roberts.1983@gmail.com>
2023-11-12 09:42:00 +1000

    Add initial codepipeline with simple source and build stages for fuzzy_cli

--- index.ts
+++ index.ts
@@ -4,8 +4,10 @@ import * as awsx from "@pulumi/awsx";
 
 import * as lambda from "./lambda";
 import * as budget from "./budget";
+import * as pipeline from "./pipeline";
 
 const budgetDefinition = budget.definition;
 const lambdaDefinition = lambda.definition;
+const pipelineDefinition = pipeline.definition;
 
 export const lambdaUrl = lambdaDefinition.functionUrl;
